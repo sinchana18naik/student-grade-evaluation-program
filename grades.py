04ad7350bd1057f71be6f838d3c48149118ad99a
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-18 23:50:49 +0530

    sve to new branch

--- grades.py
+++ grades.py
@@ -10,4 +10,20 @@ elif marks>=35:
 else:
     grade="fail"
 
+sub1=float(input("enter marks of 1st subject:"))
+
+
+sub2=float(input("enter marks of 2nd subject:"))
+
+
+sub3=float(input("enter marks of 3st subject:"))
+
+
+sub4=float(input("enter marks of 4st subject:"))
+
+
+sub5=float(input("enter marks of 5st subject:"))
+avg=(sub1+sub2+sub3+sub4+sub5)/5
+print("average of marks is :",avg)
+
 print("result is :",grade)
